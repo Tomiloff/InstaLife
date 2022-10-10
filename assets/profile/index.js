160dda50c94ae84f1e7a4b9bee70e9c236a62cdb
Vadim <Tomilov_Vadim@list.ru>
2022-10-10 22:52:42 +0500

    Added modal and API request

--- assets/profile/index.js
+++ assets/profile/index.js
@@ -21,3 +21,50 @@ let userTime = () => {
 };
 
 userTime();
+
+
+
+let btnPublication = document.getElementById("btnPublication");
+let modal = document.querySelector(".modal-profile"); 
+let popup = document.querySelector(".modal-profile-loading");
+let html = document.querySelector("html");
+let body = document.querySelector("body");
+
+btnPublication.addEventListener( "click", () => {
+
+  modal.style.display = "flex";
+  html.style.overflow = "hidden";
+  body.style.overflow = "hidden";
+
+  fetchImage();
+} );
+
+document.addEventListener( "click", (e) => {
+  let click = e.composedPath();
+
+  if (!click.includes(popup) && !click.includes(btnPublication)) {
+    modal.style.display = "none";
+    html.style.overflow = "auto";
+    body.style.overflow = "auto";
+  }
+});
+
+
+
+let btnChangePhoto = document.getElementById("changePhoto");
+let imageWrap = document.getElementById("imageWrap");
+let url = "http://aws.random.cat/meow";
+
+async function fetchImage() {
+  try {
+    let responcse = await fetch(url);
+    let data = await responcse.json();
+    imageWrap.src = data.file;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+btnChangePhoto.addEventListener( "click", () => {
+  fetchImage();
+});
