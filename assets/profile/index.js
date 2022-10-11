05fd784a431e935c0c4cc75352bcf8ab145c5f3d
Vadim <Tomilov_Vadim@list.ru>
2022-10-11 19:54:36 +0500

    Fix modal

--- assets/profile/index.js
+++ assets/profile/index.js
@@ -14,8 +14,8 @@ let userTime = () => {
       if(seconds < 10) seconds = `0${seconds}`;
 
       let clock = `${hours}:${minutes}:${seconds}`;
-      document.getElementById("clock").innerHTML = clock;
-      document.getElementById("clock-mobile").innerHTML = clock;
+      document.querySelector("#clock").innerHTML = clock;
+      document.querySelector("#clock-mobile").innerHTML = clock;
     });
   };
 };
@@ -24,14 +24,13 @@ userTime();
 
 
 
-let btnPublication = document.getElementById("btnPublication");
+let btnPublication = document.querySelector("#btnPublication");
 let modal = document.querySelector(".modal-profile"); 
 let popup = document.querySelector(".modal-profile-loading");
 let html = document.querySelector("html");
 let body = document.querySelector("body");
 
 btnPublication.addEventListener( "click", () => {
-
   modal.style.display = "flex";
   html.style.overflow = "hidden";
   body.style.overflow = "hidden";
@@ -44,15 +43,17 @@ document.addEventListener( "click", (e) => {
 
   if (!click.includes(popup) && !click.includes(btnPublication)) {
     modal.style.display = "none";
-    html.style.overflow = "auto";
-    body.style.overflow = "auto";
+    html.style.overflow = "visible";
+    body.style.overflow = "visible";
   }
 });
 
 
 
-let btnChangePhoto = document.getElementById("changePhoto");
-let imageWrap = document.getElementById("imageWrap");
+let collageContent = document.querySelector(".page-profile-content");
+let buttonChange = document.querySelector("#changePhoto");
+let buttonAddNow = document.querySelector("#addNow");
+let imageWrap = document.querySelector("#imageWrap");
 let url = "http://aws.random.cat/meow";
 
 async function fetchImage() {
@@ -65,6 +66,53 @@ async function fetchImage() {
   }
 }
 
-btnChangePhoto.addEventListener( "click", () => {
+buttonChange.addEventListener( "click", () => {
   fetchImage();
+});
+
+buttonAddNow.addEventListener( "click", () => {
+  let srcImage = imageWrap.src;
+  let newPhoto = `<div class="profile-content-wrap"><img src="${srcImage}" alt="Фото-публикация"></div>`
+
+  collageContent.insertAdjacentHTML("afterbegin", newPhoto);
+} );
+
+
+
+let buttonAddLater = document.querySelector("#addLater");
+let controlForm = document.querySelector(".loading-control-form");
+
+buttonAddLater.addEventListener( "click", () => {
+  controlForm.style.display = "flex";
+});
+
+
+let output = document.querySelector("#volume");
+
+let outputUpdate = (vol) => {
+  output.value = `Через ${vol} секунд`;
+  return vol;
+};
+
+let buttonConfirm = document.querySelector(".control-form-btn");
+let inputSlider = document.querySelector("#counter");
+
+let addPhoto = () => {
+  let srcImage = imageWrap.src;
+  let newPhoto = `<div class="profile-content-wrap"><img src="${srcImage}" alt="Фото-публикация"></div>`
+
+  collageContent.insertAdjacentHTML("afterbegin", newPhoto);
+};
+
+buttonConfirm.addEventListener( "click", () => {
+  setTimeout(addPhoto, inputSlider.value * 1000);
+  controlForm.style.display = "none";
+} );
+
+document.addEventListener( "click", (e) => {
+  let click = e.composedPath();
+
+  if (!click.includes(buttonAddLater) && !click.includes(controlForm)) {
+    controlForm.style.display = "none";
+  }
 });
