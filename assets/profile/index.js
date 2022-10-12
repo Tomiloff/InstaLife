82b8103357b644ea0b9107a83d5f618f9da97a9d
Vadim <Tomilov_Vadim@list.ru>
2022-10-12 16:11:36 +0500

    Fix index.js

--- assets/profile/index.js
+++ assets/profile/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let userTime = () => {
+let showTime = () => {
   window.onload = () => {
     this.setInterval( () => {
       let date = new Date();
@@ -20,17 +20,15 @@ let userTime = () => {
   };
 };
 
-userTime();
+showTime();
 
 
-
-let btnPublication = document.querySelector("#btnPublication");
+let btnPlus = document.querySelector("#btnPublication");
 let modal = document.querySelector(".modal-profile"); 
-let popup = document.querySelector(".modal-profile-loading");
 let html = document.querySelector("html");
 let body = document.querySelector("body");
 
-btnPublication.addEventListener( "click", () => {
+btnPlus.addEventListener( "click", () => {
   modal.style.display = "flex";
   html.style.overflow = "hidden";
   body.style.overflow = "hidden";
@@ -38,52 +36,61 @@ btnPublication.addEventListener( "click", () => {
   fetchImage();
 } );
 
+
+let popup = document.querySelector(".modal-profile-loading");
+
+let cancelStyle = () => {
+  modal.style.display = "none";
+  html.style.overflow = "visible";
+  body.style.overflow = "visible";
+};
+
 document.addEventListener( "click", (e) => {
   let click = e.composedPath();
 
-  if (!click.includes(popup) && !click.includes(btnPublication)) {
-    modal.style.display = "none";
-    html.style.overflow = "visible";
-    body.style.overflow = "visible";
-  }
+  if (!click.includes(popup) && !click.includes(btnPlus)) cancelStyle();
 });
 
 
-
-let collageContent = document.querySelector(".page-profile-content");
-let buttonChange = document.querySelector("#changePhoto");
-let buttonAddNow = document.querySelector("#addNow");
-let imageWrap = document.querySelector("#imageWrap");
+let imgWrap = document.querySelector("#imageWrap");
 let url = "http://aws.random.cat/meow";
 
 async function fetchImage() {
   try {
     let responcse = await fetch(url);
     let data = await responcse.json();
-    imageWrap.src = data.file;
+    imgWrap.src = data.file;
   } catch (error) {
     console.log(error);
   }
 }
 
-buttonChange.addEventListener( "click", () => {
+
+let btnChange = document.querySelector("#changePhoto");
+
+btnChange.addEventListener( "click", () => {
   fetchImage();
 });
 
-buttonAddNow.addEventListener( "click", () => {
-  let srcImage = imageWrap.src;
-  let newPhoto = `<div class="profile-content-wrap"><img src="${srcImage}" alt="Фото-публикация"></div>`
 
-  collageContent.insertAdjacentHTML("afterbegin", newPhoto);
+let btnNow = document.querySelector("#addNow");
+let listPhoto = document.querySelector(".page-profile-content");
+
+btnNow.addEventListener( "click", () => {
+  let srcImg = imgWrap.src;
+  let newPhoto = `<div class="profile-content-wrap"><img src="${srcImg}" alt="Фото-публикация"></div>`
+
+  listPhoto.insertAdjacentHTML("afterbegin", newPhoto);
+
+  cancelStyle();
 } );
 
 
+let btnLater = document.querySelector("#addLater");
+let timing = document.querySelector(".loading-control-form");
 
-let buttonAddLater = document.querySelector("#addLater");
-let controlForm = document.querySelector(".loading-control-form");
-
-buttonAddLater.addEventListener( "click", () => {
-  controlForm.style.display = "flex";
+btnLater.addEventListener( "click", () => {
+  timing.style.display = "flex";
 });
 
 
@@ -91,28 +98,33 @@ let output = document.querySelector("#volume");
 
 let outputUpdate = (vol) => {
   output.value = `Через ${vol} секунд`;
-  return vol;
 };
 
-let buttonConfirm = document.querySelector(".control-form-btn");
-let inputSlider = document.querySelector("#counter");
 
 let addPhoto = () => {
-  let srcImage = imageWrap.src;
-  let newPhoto = `<div class="profile-content-wrap"><img src="${srcImage}" alt="Фото-публикация"></div>`
+  let srcImg = imgWrap.src;
+  let newPhoto = `<div class="profile-content-wrap"><img src="${srcImg}" alt="Фото-публикация"></div>`
 
-  collageContent.insertAdjacentHTML("afterbegin", newPhoto);
+  listPhoto.insertAdjacentHTML("afterbegin", newPhoto);
 };
 
-buttonConfirm.addEventListener( "click", () => {
-  setTimeout(addPhoto, inputSlider.value * 1000);
-  controlForm.style.display = "none";
+
+let btnConfirm = document.querySelector(".control-form-btn");
+let inputRange = document.querySelector("#counter");
+
+btnConfirm.addEventListener( "click", () => {
+  setTimeout(addPhoto, inputRange.value * 1000);
+  
+  timing.style.display = "none";
+  
+  cancelStyle();
 } );
+
 
 document.addEventListener( "click", (e) => {
   let click = e.composedPath();
 
-  if (!click.includes(buttonAddLater) && !click.includes(controlForm)) {
-    controlForm.style.display = "none";
+  if (!click.includes(btnLater) && !click.includes(timing)) {
+    timing.style.display = "none";
   }
 });
