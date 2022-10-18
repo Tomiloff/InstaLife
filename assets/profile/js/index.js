f9e88f5b624b7a06102de0d0b5e8b846415e3ade
Vadim <Tomilov_Vadim@list.ru>
2022-10-18 22:45:28 +0500

    After code review

--- assets/profile/js/index.js
+++ assets/profile/js/index.js
@@ -2,7 +2,7 @@
 
 const showTime = () => {
   window.onload = () => {
-    this.setInterval( () => {
+    window.setInterval( () => {
       let date = new Date();
   
       let hours = date.getHours();
@@ -23,50 +23,7 @@ const showTime = () => {
 showTime();
 
 
-const posts = [
-  {
-    id: 1, 
-    login: 'name', 
-    description: 'Картина дерева', 
-    src: '../assets/profile/images/img-content-1.jpg',
-    like: 18
-  },
-  {
-    id: 2, 
-    login: 'name', 
-    description: 'В мире животных', 
-    src: '../assets/profile/images/img-content-2.jpg',
-    like: 5
-  },
-  {
-    id: 3, 
-    login: 'name', 
-    description: 'Невероятная архитектура здания Венгерского парламента', 
-    src: '../assets/profile/images/img-content-3.jpg',
-    like: 10
-  },
-  {
-    id: 4, 
-    login: 'name', 
-    description: 'Клубничный торт на Ваш праздник', 
-    src: '../assets/profile/images/img-content-4.jpg',
-    like: 69
-  },
-  {
-    id: 5, 
-    login: 'name', 
-    description: 'Всё для сада и огорода', 
-    src: '../assets/profile/images/img-content-5.jpeg',
-    like: 34
-  },
-  {
-    id: 6, 
-    login: 'name', 
-    description: 'Силуэт дерева с раскидистой кроной и с солнцем по центру', 
-    src: '../assets/profile/images/img-content-6.jpg',
-    like: 78
-  },
-];
+import {posts} from "./funds.js";
 
 
 const listPhoto = document.querySelector(".page-profile-content");
@@ -74,14 +31,10 @@ const listPhoto = document.querySelector(".page-profile-content");
 const showPosts = () => {
   listPhoto.innerHTML = "";
 
-  let count = 1;
-
   posts.map( (index) => {
-    let newPhoto = `<div id="${count}" class="profile-content-wrap">
+    let newPhoto = `<div id="${index.id}" class="profile-content-wrap">
                     <img src="${index.src}" 
                     alt="Фото-публикация"></div>`;
-
-    count++;
 
     listPhoto.insertAdjacentHTML("afterbegin", newPhoto);
   } );
